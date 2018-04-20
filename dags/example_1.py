0bd8a69655a284da488f329b84403c41fefebdae
Kaxil Naik <kaxilnaik@gmail.com>
2018-04-20 15:48:16 +0100

    Formatting change

--- dags/example_1.py
+++ dags/example_1.py
@@ -7,7 +7,6 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 
-
 def print_world():
     print('Welcome to Airflow tutorial at Pydata London 2018')
 
@@ -24,26 +23,24 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-
 with DAG('airflow_tutorial_v01',
          default_args=default_args,
          # schedule_interval='0 * * * *',
          ) as dag:
-
     print_hello = BashOperator(
-                    task_id='print_hello',
-                    bash_command='echo "hello"'
-                    )
+        task_id='print_hello',
+        bash_command='echo "hello"'
+    )
 
     sleep = BashOperator(
-                    task_id='sleep',
-                    bash_command='sleep 5'
-                    )
+        task_id='sleep',
+        bash_command='sleep 5'
+    )
 
     print_world = PythonOperator(
-                    task_id='print_world',
-                    python_callable=print_world
-                    )
+        task_id='print_world',
+        python_callable=print_world
+    )
 
 # one way of setting dependencies
 print_hello >> sleep >> print_world
@@ -51,4 +48,3 @@ print_hello >> sleep >> print_world
 # other way of setting dependencies
 # sleep.set_upstream(print_hello) # Equivalent to print_hello.set_downstream(sleep)
 # print_world.set_upstream(sleep) # Equivalent to sleep.set_downstream(print_world)
- 
--- dags/example_2.py
+++ dags/example_2.py
@@ -1,7 +1,7 @@
 # import in-built module 
 import sys
 import time
-import os 
+import os
 import csv
 
 # import airflow module and operator
@@ -17,47 +17,47 @@ input_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'example_c
 dag_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.today(), 'email_on_failure': False,                
+    'start_date': datetime.today(), 'email_on_failure': False,
     'retries': 1, 'retry_delay': timedelta(minutes=2)
 }
 
 dag = DAG(
-    dag_id='Dynamic_dag_example', 
+    dag_id='Dynamic_dag_example',
     default_args=dag_args,
-    )
+)
 
-i = 0 # Counter for dummy variable 
+i = 0  # Counter for dummy variable
 
-with open(input_file,'r') as f:
+with open(input_file, 'r') as f:
     reader = csv.reader(f)
     for line in reader:
         taskid = line[0]
         argument = line[1]
         type_arg = line[2]
         t1 = DummyOperator(
-                task_id='dummy_dag_{}'.format(i),
-                dag=dag
-                )
+            task_id='dummy_dag_{}'.format(i),
+            dag=dag
+        )
         i = i + 1
         t2 = DummyOperator(
-                task_id='dummy_dag_{}'.format(i),
-                dag=dag
-                )
+            task_id='dummy_dag_{}'.format(i),
+            dag=dag
+        )
 
         if type_arg == 'slp':
             myBashTask = BashOperator(
                 task_id='task_sleep_{}'.format(i),
                 bash_command='sleep {{ sleep }}',
-                params={'sleep' : int(argument)},
+                params={'sleep': int(argument)},
                 dag=dag)
         elif type_arg == 'ech':
             myBashTask = BashOperator(
                 task_id='task_echo_{}'.format(i),
                 bash_command='echo {{ prt }}',
-                params={'prt' : str(argument)},
+                params={'prt': str(argument)},
                 dag=dag)
 
-        t1 >> myBashTask >> t2        
+        t1 >> myBashTask >> t2
         # myBashTask.set_upstream(t1)
         # t2.set_upstream(myBashTask)
 
